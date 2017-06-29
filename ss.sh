688e71f8b634720ea821ac4469b3a704e6ab3992
Clay Mullis <ovenandtap@gmail.com>
2017-06-29 17:45:51 -0500

    Small adjustments to script.

--- ss.sh
+++ ss.sh
@@ -14,26 +14,31 @@ do
   f_lower=`echo "$f" | tr '[:upper:]' '[:lower:]'`
   if [[ $f_lower == *"brunch"* ]]; then
     if [[ $f_lower == *"beverage"* ]]; then
-      cp "$f" renamed/brunch_drinks_menu.pdf
+      cp "$f" renamed/brunch_drinks_menu.pdf;
     else
-      cp "$f" renamed/brunch_menu1.pdf
+      cp "$f" renamed/brunch_menu.pdf;
     fi
   else
     if [[ $f_lower == *"dinner"* ]]; then
       if [[ $f_lower == *"beverage"* ]]; then
-        cp "$f" renamed/dinner_drinks_menu.pdf
+        cp "$f" renamed/dinner_drinks_menu.pdf;
       else
-        cp "$f" renamed/dinner_menu.pdf
+        cp "$f" renamed/dinner_menu.pdf;
       fi
     else
-      echo "Bad filename input. Abort."
-      exit 0
+      if [[ $f_lower == *"lunch"* ]]; then
+        if [[ $f_lower == *"beverage"* ]]; then
+          cp "$f" renamed/lunch_drinks_menu.pdf;
+        else
+          cp "$f" renamed/lunch_menu.pdf;
+        fi
+      fi
     fi
   fi
 done
 
-mkdir converted
 # Convert PDFs to PNGs
+mkdir converted
 for f in renamed/*.pdf ; do convert -density 150 "$f" -quality 70 "${f%.*}.png" ; done
 mkdir pngquant-files
 mv renamed/*.png pngquant-files;
